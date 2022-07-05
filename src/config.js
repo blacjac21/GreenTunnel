beb555b766d9ba912c6a124aa0334a45fccfc07a
blacjac21 <79352087+blacjac21@users.noreply.github.com>
2022-07-06 00:41:46 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
-	ip: '127.0.0.1',
+	ip: '192.168.1.101',
 	port: 8000,
 	httpsOnly: false,
 	clientHelloMTU: 100,
 	dns: {
 		type: 'https', // 'tls' or 'https' or 'unencrypted'
 		server: 'https://cloudflare-dns.com/dns-query',
-		ip: '127.0.0.1',
+		ip: '192.168.1.101',
 		port: 53,
 		cacheSize: 1000,
 	}
